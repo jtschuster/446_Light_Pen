deb1d822b9dcbfc5c66177b2d29fbab006d320fe
jtschuster <jacksontschuster@gmail.com>
2020-04-29 13:26:17 -0500

    added framebuffer test

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
 	gcc x11.c -o x11 -lX11 
+i2c:
+	gcc i2c.c -o i2c
 clean:
-	rm x11
+	rm x11 i2c
--- i2c.c
+++ i2c.c
@@ -2,7 +2,7 @@
 #include <fcntl.h>				//Needed for I2C port
 #include <sys/ioctl.h>			//Needed for I2C port
 #include <linux/i2c-dev.h>		//Needed for I2C port
-
+#include <stdio.h>
 int file_i2c;
 int length;
 unsigned char buffer[60] = {0};
@@ -15,17 +15,26 @@ int main() {
     {
         //ERROR HANDLING: you can check errno to see what went wrong
         printf("Failed to open the i2c bus");
-        return;
+        return 0;
     }
 
-    int addr = 0x5a;          //<<<<<The I2C address of the slave
+    int addr = 0x49;          //<<<<<The I2C address of the slave
     if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
     {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         //ERROR HANDLING; you can check errno to see what went wrong
-        return;
+        return 0;
     }
-
+    //----- WRITE BYTES -----
+    buffer[0] = 0x02;
+    buffer[1] = 0x08;
+    length = 2;			//<<< Number of bytes to write
+    if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+    {
+        /* ERROR HANDLING: i2c transaction failed */
+        printf("Failed to write to the i2c bus.\n");
+    }
+ 
 
     //----- READ BYTES -----
     length = 4;			//<<< Number of bytes to read
@@ -36,17 +45,9 @@ int main() {
     }
     else
     {
-        printf("Data read: %s\n", buffer);
+        printf("Data read: %x\n", *buffer);
     }
 
 
-    //----- WRITE BYTES -----
-    buffer[0] = 0x01;
-    buffer[1] = 0x02;
-    length = 2;			//<<< Number of bytes to write
-    if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-    {
-        /* ERROR HANDLING: i2c transaction failed */
-        printf("Failed to write to the i2c bus.\n");
-    }
+   return 0;
 }
