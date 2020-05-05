a8f2a5c0539d2b2295b9be8f407196b1737b9953
jtschuster <jacksontschuster@gmail.com>
2020-05-05 14:50:23 -0500

    beginning brightness changing

--- fb_test/fb_test.c
+++ fb_test/fb_test.c
@@ -1,69 +1,99 @@
-#include <linux/fb.h>
-#include <stdio.h>
-#include <stdint.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <linux/fb.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 
-uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo)
+#define BOX_WIDTH 10
+#define BOX_HEIGHT 10
+#define MIN_CHANGE 0x0a
+
+uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b,
+    struct fb_var_screeninfo* vinfo)
 {
-	return (r<<vinfo->red.offset) | (g<<vinfo->green.offset) | (b<<vinfo->blue.offset);
+    return (r << vinfo->red.offset) | (g << vinfo->green.offset) | (b << vinfo->blue.offset);
 }
 
 int main()
 {
-	struct fb_fix_screeninfo finfo;
-	struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    struct fb_var_screeninfo vinfo;
 
-	int fb_fd = open("/dev/fb0",O_RDWR);
+    int fb_fd = open("/dev/fb0", O_RDWR);
 
-	//Get variable screen information
-	ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
-	vinfo.grayscale=0;
-	vinfo.bits_per_pixel=32;
-	ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vinfo);
-	ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
+    // Get variable screen information
+    ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
+    vinfo.grayscale = 0;
+    vinfo.bits_per_pixel = 32;
+    ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vinfo);
+    ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
 
-	ioctl(fb_fd, FBIOGET_FSCREENINFO, &finfo);
+    ioctl(fb_fd, FBIOGET_FSCREENINFO, &finfo);
 
-	long screensize = vinfo.yres_virtual * finfo.line_length;
+    long screensize = vinfo.yres_virtual * finfo.line_length;
 
-	uint8_t *fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+    uint8_t* fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
 
-	int x,y;
+    int x, y;
+    // dividend plus 1 if remainder
+    int rows = vinfo.yres / BOX_HEIGHT + !!(vinfo.yres % BOX_HEIGHT);
+    int cols = vinfo.xres / BOX_WIDTH + !!(vinfo.xres % BOX_WIDTH);
+    // index with [x][y]
+	// msb indicated 
+    uint32_t curr_brightness[cols][rows];
+    int column = 0;
+    int row = 0;
 
-	for (x=0;x<vinfo.xres;x++)
-		for (y=0;y<vinfo.yres;y++)
-		{
-			long location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (y+vinfo.yoffset) * finfo.line_length;
-			uint32_t color = *((uint32_t*)(fbp + location));
-			int32_t red = ((0xFF << vinfo.red.offset) & color) >> vinfo.red.offset;
-			int32_t blue = ((0xFF << vinfo.blue.offset) & color)>> vinfo.blue.offset;
-			int32_t green = ((0xFF << vinfo.green.offset) & color) >> vinfo.green.offset;
-                        int total = green + blue + red;
-                        int dgreen=0;
-                        int dred=0;
-                        int dblue=0;
-                        if (total > 382) { // half of white
-		    	    if (green <= 0x0f) {
-                                dgreen = -green;
-		    	    } else {
-                                dgreen = -0x0F;
-                            }
-		    	    if (red <= 0x0F) {
-                                dred = -red;
-		    	    } else {
-                                dred = -0x0F;
-                            }
-		    	    if (blue <= 0x0F) {
-                                dblue = -blue;
-		    	    } else {
-                                dblue = -0x0F;
-                            }
-		        }	
-			color -= pixel_color(dred, dblue, dred, &vinfo);
-			*((uint32_t*)(fbp + location)) = color;
-		}
-
-	return 0;
+    for (x = 0; x < vinfo.xres; x++) {
+        column = x / BOX_WIDTH;
+        for (y = 0; y < vinfo.yres; y++) {
+            row = y / BOX_HEIGHT;
+            unsigned long location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
+            uint32_t color = *((uint32_t*)(fbp + location));
+            uint32_t red = ((0xFF << vinfo.red.offset) & color) >> vinfo.red.offset;
+            uint32_t blue = ((0xFF << vinfo.blue.offset) & color) >> vinfo.blue.offset;
+            uint32_t green = ((0xFF << vinfo.green.offset) & color) >> vinfo.green.offset;
+            unsigned int total = green + blue + red;
+            curr_brightness[column][row] += total;
+            // int dgreen = 0;
+            // int dred = 0;
+            // int dblue = 0;
+            // if (total > 382)
+            // { // half of white
+            // 	if (green <= 0x0f)
+            // 	{
+            // 		dgreen = -green;
+            // 	}
+            // 	else
+            // 	{
+            // 		dgreen = -0x0F;
+            // 	}
+            // 	if (red <= 0x0F)
+            // 	{
+            // 		dred = -red;
+            // 	}
+            // 	else
+            // 	{
+            // 		dred = -0x0F;
+            // 	}
+            // 	if (blue <= 0x0F)
+            // 	{
+            // 		dblue = -blue;
+            // 	}
+            // 	else
+            // 	{
+            // 		dblue = -0x0F;
+            // 	}
+        }
+    }
+    for (x = 0; x < vinfo.xres; x++) {
+        column = x / BOX_WIDTH;
+        for (y = 0; y < vinfo.yres; y++) {
+            row = y / BOX_HEIGHT;
+            color -= pixel_color(dred, dblue, dred, &vinfo);
+            *((uint32_t*)(fbp + location)) = color;
+        }
+    }
+    return 0;
 }
